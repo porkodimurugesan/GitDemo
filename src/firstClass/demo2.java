bdd686ce6a929569dadad0ae386a8588d64a3def
porkodimurugesan <porks2903@gmail.com>
2024-11-05 20:10:21 +0530

    Final commit

---
+++ src/firstClass/demo2.java
@@ -0,0 +1,12 @@
+package firstClass;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		
+		System.out.print("GitHub tutorial");
+	}
+
+}
