69d91c0e98d9463ffc0737b82ef57d15d7ad6b12
porkodimurugesan <porks2903@gmail.com>
2024-11-05 19:51:11 +0530

    code changes by onshore qa

--- src/firstClass/demo.java
+++ src/firstClass/demo.java
@@ -5,6 +5,8 @@ public class demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		
+		System.out.print("GitHub tutorial");
 	}
 
 }
